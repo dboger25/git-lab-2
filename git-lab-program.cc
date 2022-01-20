0b5a5f5ca41001dceb27c219db6458b694ef440b
dboger25 <db165418@ohio.edu>
2022-01-19 19:40:23 -0500

    uploading

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Dawson Boger
+ *        Date: 01/19/22
+ * Description: Some code
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
